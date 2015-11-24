7b2d337f95455c6b5bd68de4acb1014aea6b24d7
Luciana <staticbangs@gmail.com>
2015-11-24 19:44:50 +0100

    Personajeanda

--- jijiTeam_Unity/Assets/Scripts/Movimiento.cs
+++ jijiTeam_Unity/Assets/Scripts/Movimiento.cs
@@ -4,14 +4,20 @@ using System.Collections;
 public class Movimiento : MonoBehaviour {
 
 	public float fuerzaSalto =200f;
-	public float velocidad_pollo = 50f;
+	public float velocidad = 50f;
 	private int numsaltos=0;
 	Rigidbody2D rg; //se le puede dar el nombre que quieras, en este caso rg
+	private Animator anim;
+	private Vector3 miraDerecha;
+	private Vector3 miraIzquierda;
 
 	// Use this for initialization
 	void Start () {
+
 		rg= GetComponent<Rigidbody2D>();
-	
+		anim = GetComponent<Animator>();
+		miraIzquierda = new Vector3(-transform.localScale.x,transform.localScale.y,transform.localScale.z);
+		miraDerecha = transform.localScale;
 	}
 	
 	// Update is called once per frame
@@ -45,6 +51,9 @@ public class Movimiento : MonoBehaviour {
 			mueve_derecha();
 		}
 		Vector2 velocidad = GetComponent<Rigidbody2D>().velocity;
+
+		anim.SetFloat("caminando", Mathf.Abs(velocidad.x));
+
 	Debug.DrawLine(transform.position, new Vector3(transform.position.x+ velocidad.x,
 		                                           transform.position.y + velocidad.y, transform.position.z));
 	}
@@ -56,16 +65,16 @@ public class Movimiento : MonoBehaviour {
 		
 		void mueve_izquierda()
 	{
-		transform.localScale=(new Vector3(1,1,1));
+		transform.localScale=miraIzquierda;
 		//rg.AddForce(new Vector2 (-(velocidad_pollo),0)); con esto lo que pasa esq a cada frame se le da esa velocidad y sale disparado
-		rg.velocity=(new Vector2 (-(velocidad_pollo),rg.velocity.y)); //de esta forma la velocidad siempre sera constante, el rg.velocity.y es para q caiga a la vez que salta
+		rg.velocity=(new Vector2 (-(velocidad),rg.velocity.y)); //de esta forma la velocidad siempre sera constante, el rg.velocity.y es para q caiga a la vez que salta
 
 	}
 		void mueve_derecha()
 	{
-		transform.localScale=(new Vector3(-1,1,1));
+		transform.localScale=miraDerecha;
 		//rg.AddForce(new Vector2 (velocidad_pollo,0 ));
-		rg.velocity=(new Vector2 (velocidad_pollo,rg.velocity.y));
+		rg.velocity=(new Vector2 (velocidad,rg.velocity.y));
 
 		}
 
